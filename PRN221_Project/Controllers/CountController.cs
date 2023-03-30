39fbfcff6a4d1e6c6be32bc6cd2d7fe8f42c58a6
MQuan <nmquanvip2@gmail.com>
2023-03-30 13:34:45 +0700

    add calculator

---
+++ PRN221_Project/Controllers/CountController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Expense_Tracker.Controllers
+{
+    public class CountController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult Index(float money, float rate, int month)
+        {
+            double result = Math.Round(money * Math.Pow((1 + rate/100),month),2);
+            return View(result);
+        }
+    }
+}
